6833b2d6c5e65378ee8faa93d67ec0028288a59a
Jim <wefactoryou@gmail.com>
2018-03-28 18:39:07 -0700

    update devise views and workout views

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,11 +1,25 @@
 class WorkoutsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_workout, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @workouts = Workout.all.order("created_at DESC")
+  end
+  
+  def show
   end
   
   def new
+    @workout = Workout.new
   end
   
   def create
+    @workout = Workout.new(workout_params)
+    if @workout.save
+      redirect_to @workout
+    else
+      render 'new'
+    end
   end
   
   def edit
@@ -15,5 +29,15 @@ class WorkoutsController < ApplicationController
   end
   
   def destroy
+  end
+  
+  private
+  
+  def workout_params
+    params.require(:workout).permit(:date, :workout, :content, :length)
+  end
+  
+  def find_workout
+    @workout = Workout.find(params[:id])
   end
 end
